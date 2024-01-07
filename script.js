a820f72112c1cc0cfde1b1669023f2e3b3633bc0
Ashif <144139175+Ashif-107@users.noreply.github.com>
2024-01-07 13:47:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+/*=========== responsive nav ==========*/
+
+var navMenu = document.querySelector('.side-nav')
+
+function shownavbar(){
+    navMenu.style.right="0"
+}
+function closenavbar(){
+    navMenu.style.right="-100%"
+}
+
+/*======active section========== */
+
+
+/*========== swiper ==========*/
+
+const swipershoe= new Swiper('.home-img', {
+    
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+    effect: 'creative',
+    creativeEffect:{
+        prev: {
+            translate: [-100,0,-500],
+            rotate: [0,0,15],
+            opacity: 0
+        },
+        next: {
+            translate: [100,0,-500],
+            rotate: [0,0,-15],
+            opacity: 0
+        },
+    },
+    autoplay: {
+        delay: 1500, 
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+})
